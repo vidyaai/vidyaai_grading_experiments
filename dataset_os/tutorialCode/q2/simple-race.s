f4b540b19fd94a49b4c6c97ce2e9eb5bcede5ff0
wenjing1170 <111716787+wenjing1170@users.noreply.github.com>
2024-05-29 21:51:24 +0800

    Add files via upload

---
+++ dataset_os/tutorialCode/q2/simple-race.s
@@ -0,0 +1,6 @@
+.main
+# this is a critical section
+mov 2000, %ax   # get the value at the address
+add $1, %ax 	# increment it
+mov %ax, 2000   # store it back
+halt
---
+++ dataset_os/tutorialCode/q2/t0.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *mythread(void *arg) {
+    printf("%s\n", (char *) arg);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 1) {
+	fprintf(stderr, "usage: main\n");
+	exit(1);
+    }
+
+    pthread_t p1, p2;
+    printf("main: begin\n");
+    pthread_create(&p1, NULL, mythread, "A");
+    pthread_create(&p2, NULL, mythread, "B");
+    // join waits for the threads to finish
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    printf("main: end\n");
+    return 0;
+}
+
